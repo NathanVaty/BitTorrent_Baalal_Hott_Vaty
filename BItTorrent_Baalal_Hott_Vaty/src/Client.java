17248c471a4f754b2c5af5c3dfb6006d1e6d7ab9
NathanVaty <45233047+NathanVaty@users.noreply.github.com>
2020-01-28 22:24:50 +0100

    Client/serveur transfert de fichiers soucis de lecture du buffer

--- BItTorrent_Baalal_Hott_Vaty/src/Client.java
+++ BItTorrent_Baalal_Hott_Vaty/src/Client.java
@@ -1,8 +1,18 @@
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*
@@ -16,36 +26,150 @@ import java.util.Scanner;
  * @author Nathan Vaty
  */
 public class Client {
-    public static void main(String args[]) throws Exception {
+    public static void main(String args[]) throws IOException {
 		// Création d'un socket client et connexion avec un serveur fonctionnant sur la même machine et sur le port 40000
 		Socket sc = new Socket("localhost", 40000);
 		
 		// Construction d'un BufferedReader pour lire du texte envoyé à travers la connexion socket
 		BufferedReader entreeSocket = new BufferedReader(new InputStreamReader(sc.getInputStream()));
 		// Construction d'un PrintStream pour envoyer du texte à travers la connexion socket
-		PrintStream sortieSocket = new PrintStream(sc.getOutputStream());
-					
+		BufferedOutputStream sortieSocket = new BufferedOutputStream(sc.getOutputStream());
+                
+                PrintStream sortieNomFich = new PrintStream(sc.getOutputStream());
+                
+//                 BufferedReader fichier = new BufferedReader(new FileReader("C:"
+//                         + "\\Users\\vanat\\OneDrive\\Documents"
+//                        + "\\NetBeansProjects\\BitTorrent_Baalal_Hott_Vaty"
+//                        + "\\BItTorrent_Baalal_Hott_Vaty\\src\\srcClient\\fonddecrantropbeau.jpg"));
+//				
+//                 FileInputStream fich = new FileInputStream(new File("C:"
+//                         + "\\Users\\vanat\\OneDrive\\Documents"
+//                        + "\\NetBeansProjects\\BitTorrent_Baalal_Hott_Vaty"
+//                        + "\\BItTorrent_Baalal_Hott_Vaty\\src\\srcClient\\testSrc.txt"));
+                 
+                
+                  
+                  //FileOutputStream sortie = new FileOutputStream(new File(sc.getOutputStream()));
+                 
+                 
 		String chaine = "";
+                String cheminFich;
+                byte[] buffer = new byte[250];
+                int n=0;
 		
-		// Scanner sur System.in
-		Scanner scanner = new Scanner(System.in);
+                Scanner entree = new Scanner(System.in);
+                
+                System.out.print("Entrez le chemin d'accès au dossier du fichier: ");
+                cheminFich = entree.nextLine()+"\\";
+                System.out.println(cheminFich);
 		
-		System.out.println("Tapez vos phrases ou exit pour arrêter:");
-					
-		while(!chaine.equalsIgnoreCase("exit")) {
-			// lecture clavier
-			chaine = scanner.nextLine();
-			sortieSocket.println(chaine); // on envoie la chaine au serveur
-			
-			// lecture d'une chaine envoyée à travers la connexion socket
-			chaine = entreeSocket.readLine();
-			System.out.println("Chaine reçue : "+chaine);
-		}
-		
+		//System.out.println("Tapez vos phrases ou exit pour arrêter:");
+                System.out.print("Entrez le nom du fichier avec son extension: ");
+                chaine = entree.nextLine();
+                System.out.println(chaine);
+                
+                cheminFich += chaine;
+                System.out.print(cheminFich);
+                    
+                
+                FileInputStream fichierSrc = new FileInputStream(new File(cheminFich));
+                  
+                sortieNomFich.println(chaine);
+                
+                while ((n=fichierSrc.read(buffer))>0) {
+                   
+                    sortieSocket.write(buffer,0,n);
+                    //sortieSocket.flush(); // flush si texte < 250 caract
+                    System.out.println(buffer);
+                   
+                }
+
 		// on ferme nous aussi la connexion
 		sc.close();
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- BItTorrent_Baalal_Hott_Vaty/src/Serveur.java
+++ BItTorrent_Baalal_Hott_Vaty/src/Serveur.java
@@ -1,11 +1,15 @@
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -30,34 +34,129 @@ public class Serveur {
 			Socket sss = ssg.accept();
 			
 			// Construction d'un BufferedReader pour lire du texte envoyé à travers la connexion socket
-			BufferedReader entreeSocket = new BufferedReader(new InputStreamReader(sss.getInputStream()));
-			// Construction d'un PrintStream pour envoyer du texte à travers la connexion socket
-			PrintStream sortieSocket = new PrintStream(sss.getOutputStream());
+			BufferedInputStream entreeSocket = new BufferedInputStream(sss.getInputStream());
+                        BufferedReader entreeNomFich = new BufferedReader(new InputStreamReader(sss.getInputStream()));
                         
-                        FileOutputStream fichier = new FileOutputStream(new File("dstServeur/testServ.txt"));
+                        //DataInputStream entree = new DataInputStream(sss.getInputStream());
+                        
+			// Construction d'un PrintStream pour envoyer du texte à travers la connexion socket
+			//PrintStream sortieSocket = new PrintStream(sss.getOutputStream());
+                        
+//                        FileOutputStream fichier = new FileOutputStream(new File("C:\\Users\\vanat\\OneDrive\\Documents"
+//                                + "\\NetBeansProjects\\BitTorrent_Baalal_Hott_Vaty\\BItTorrent_Baalal_Hott_Vaty"
+//                                + "\\src\\dstServeur\\testServ.txt"));
+                        
+                        String destFich = "C:\\BitTorrentBHV\\";
+                        File dossier = new File(destFich);
+                        boolean estCree = dossier.mkdirs();
+                        String nomFich="";
 			byte[] buffer = new byte[250];
                         char[] bufferChar = new char[250];
+                       
                         
 			String chaine = "";
 			int n = 0;
-			while((n = entreeSocket.read(bufferChar)) >= 0) {
-				// lecture d'une chaine envoyée à travers la connexion socket
-				//chaine = entreeSocket.readLine();
-                               // Convertir char[] en byte[] buffer = toBytes(bufferChar);
-                                fichier.write(buffer);
+                      
+                        
+                        if (chaine != null) {
+                            chaine = entreeNomFich.readLine();
+                            System.out.println(chaine.length());
+                            nomFich = chaine;
+                            System.out.println(chaine);
+                            System.out.println(nomFich);
+                        FileOutputStream fichDst = new FileOutputStream(new File(destFich+nomFich));
+                        
+			while((n = entreeSocket.read(buffer)) >= 0) {
+                           System.out.print((buffer));
+                           System.out.print("Oui");
+                           fichDst.write(buffer,0,n);
 				
-				// si elle est nulle c'est que le client a fermé la connexion
-				if (chaine != null)
-                                        System.out.println(chaine);
-					sortieSocket.println(chaine); // on envoie la chaine au client
 			}
-			
+			System.out.print("non");
+                        fichDst.close();
+                        } else {
+                            System.err.println("Erreur nomFich");
+                        }
+                        
 			// on ferme nous aussi la connexion
 			sss.close();
+                        
 		}
 	}
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
