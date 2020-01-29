7ffab1953922e4d2dbc781b9a58fa47ee9a4eb7a
NathanVaty <45233047+NathanVaty@users.noreply.github.com>
2020-01-29 14:59:43 +0100

    Client/serveur bitTorrent v1 fonctionnel avec commentaires

--- BItTorrent_Baalal_Hott_Vaty/src/Client.java
+++ BItTorrent_Baalal_Hott_Vaty/src/Client.java
@@ -1,168 +1,79 @@
 
-import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.Socket;
-import java.util.Arrays;
 import java.util.Scanner;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 
 /**
  *
- * @author Nathan Vaty
+ * @author Vaty, Hott, Baalal
  */
+
 public class Client {
     public static void main(String args[]) throws IOException {
-		// Création d'un socket client et connexion avec un serveur fonctionnant sur la même machine et sur le port 40000
-		Socket sc = new Socket("localhost", 40000);
-                Socket scom = new Socket("localhost", 39000); 
-		
-		// Construction d'un BufferedReader pour lire du texte envoyé à travers la connexion socket
-		BufferedReader entreeSocket = new BufferedReader(new InputStreamReader(sc.getInputStream()));
-		// Construction d'un PrintStream pour envoyer du texte à travers la connexion socket
-		BufferedOutputStream sortieSocket = new BufferedOutputStream(scom.getOutputStream());
+        // Création d'un socket client pour la communication du nom de fichier
+        Socket sFich; 
+        
+        // Création d'un socket pour la communication des données binaires
+        Socket sBin; 
+        
+        try {
+            sFich = new Socket("192.168.43.128", 40000);
+            sBin = new Socket("192.168.43.128", 39000);
+            
+            BufferedOutputStream sortieSocket = new BufferedOutputStream(sBin.getOutputStream());
+            
+            // Construction d'un PrintStream pour envoyer le nom du fichier au serveur
+            PrintStream sortieNomFich = new PrintStream(sFich.getOutputStream());
+            
+            String chaine;
+            String cheminFich;
+            byte[] buffer = new byte[250];
+            int n;
+            
+            Scanner entree = new Scanner(System.in);
+            
+            System.out.print("Entrez le chemin d'accès au dossier du fichier: ");
+            cheminFich = entree.nextLine()+"\\";
+            
+            
+            //System.out.println("Tapez vos phrases ou exit pour arrêter:");
+            System.out.print("Entrez le nom du fichier avec son extension: ");
+            chaine = entree.nextLine();
+            cheminFich += chaine;
+            System.out.print(cheminFich);
+            
+            // Construction du FileInputStream qui permet d'envoyer les données binaires au serveur
+            FileInputStream fichierSrc = new FileInputStream(new File(cheminFich));
+            
+            sortieNomFich.print(chaine);
+            
+            while ((n=fichierSrc.read(buffer))>0) {
                 
-                PrintStream sortieNomFich = new PrintStream(sc.getOutputStream());
-                 
-		String chaine = "";
-                String cheminFich;
-                byte[] buffer = new byte[250];
-                int n=0;
-		
-                Scanner entree = new Scanner(System.in);
+                sortieSocket.write(buffer,0,n);
+                sortieSocket.flush(); // flush si texte < 250 caract
+                System.out.println(buffer);
                 
-                System.out.print("Entrez le chemin d'accès au dossier du fichier: ");
-                cheminFich = entree.nextLine()+"\\";
-                System.out.println(cheminFich);
-		
-		//System.out.println("Tapez vos phrases ou exit pour arrêter:");
-                System.out.print("Entrez le nom du fichier avec son extension: ");
-                chaine = entree.nextLine();
-                System.out.println(chaine);
-                
-                cheminFich += chaine;
-                System.out.print(cheminFich);
-                    
-                
-                FileInputStream fichierSrc = new FileInputStream(new File(cheminFich));
-                  
-                sortieNomFich.print(chaine);
-                
-                while ((n=fichierSrc.read(buffer))>0) {
-                   
-                    sortieSocket.write(buffer,0,n);
-                    sortieSocket.flush(); // flush si texte < 250 caract
-                    System.out.println(buffer);
-                   
-                }
-
-		// on ferme nous aussi la connexion
-		sc.close();
-                scom.close();
-	}
+            }
+            
+            // Le fichier est bien envoyé
+            System.out.println("Fichier bien transmis");
+            // on ferme nous aussi la connexion
+            sFich.close();
+            sBin.close();
+        } catch(IOException e) {
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- BItTorrent_Baalal_Hott_Vaty/src/Serveur.java
+++ BItTorrent_Baalal_Hott_Vaty/src/Serveur.java
@@ -1,161 +1,86 @@
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.ByteBuffer;
-import java.util.Arrays;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 
 /**
  *
- * @author Nathan Vaty
+ * @author Baalal,Hott,Vaty
  */
 public class Serveur {
-    public static void main(String args[]) throws Exception {	
-		
-		// Création d'un socket serveur générique sur le port 40000
-		ServerSocket ssg = new ServerSocket(40000);
+    public static void main(String args[]) throws Exception {
+        
+        // Création d'un socket pour le nom du fichier sur le port 40000
+        ServerSocket ssF = new ServerSocket(40000);
+        
+        // Création d'un socket pour les données binaires
+        ServerSocket ssB = new ServerSocket(39000);
+        
+        
+        while(true) {
+            // On attend une connexion puis on l'accepte
+            Socket sssF;
+            Socket sssB;
+            try {
+                sssF = ssF.accept();
+                sssB = ssB.accept();
                 
-                ServerSocket sscom = new ServerSocket(39000);
+                // Construction d'un BufferedInputStream pour lire les données binaires à copier
+                BufferedInputStream entreeSocket = new BufferedInputStream(sssB.getInputStream());
                 
-		
-		while(true) {
-			// On attend une connexion puis on l'accepte
-			Socket sss = ssg.accept();
-                        Socket sscoms = sscom.accept();
-			
-			// Construction d'un BufferedReader pour lire du texte envoyé à travers la connexion socket
-			BufferedInputStream entreeSocket = new BufferedInputStream(sscoms.getInputStream());
-                        BufferedReader entreeNomFich = new BufferedReader(new InputStreamReader(sss.getInputStream()));
-                        
-                        String destFich = "C:\\BitTorrentBHV\\";
-                        File dossier = new File(destFich);
-                        boolean estCree = dossier.mkdirs();
-                        String nomFich="";
-			byte[] buffer = new byte[250];
-                        char[] bufferChar = new char[250];
-                       
-                        
-			String chaine = "";
-			int n = 0;
-                      
-                        
-                        if (chaine != null) {
-                            chaine = entreeNomFich.readLine();
-                            System.out.println(chaine.length());
-                            nomFich = chaine;
-                            System.out.println(chaine);
-                        } else {
-                            System.err.println("Erreur nomFich");
-                        }
-                        System.out.println(nomFich);
-                        FileOutputStream fichDst = new FileOutputStream(new File(destFich+nomFich));
-                        
-			while((n = entreeSocket.read(buffer)) >= 0) {
-                           System.out.print((buffer));
-                           System.out.print("Oui");
-                           fichDst.write(buffer,0,n);
-				
-			}
-			System.out.print("non");
-			// on ferme nous aussi la connexion
-			sss.close();
-                        sscoms.close();
-                        fichDst.close();
-		}
-	}
+                // Construction d'un BufferedReader pour lire le nom du fichier envoyé à travers la connexion socket
+                BufferedReader entreeNomFich = new BufferedReader(new InputStreamReader(sssF.getInputStream()));
+                
+                // On créé le fichier BitTorrentBHV à la racine du disque C pour stocker le fichier
+                String destFich = "C:\\BitTorrentBHV\\";
+                File dossier = new File(destFich);
+                boolean estCree = dossier.mkdirs();
+                String nomFich="";
+                byte[] buffer = new byte[250];
+                
+                String chaine = "";
+                int n = 0;
+                
+                if (chaine != null) {
+                    // On lit le nom du fichier à copier
+                    chaine = entreeNomFich.readLine();
+                    nomFich = chaine;
+                    
+                } else {
+                    System.err.println("Erreur nomFich");
+                }
+                // On crée le fichier destination dans le dossier BitTorrentBHV
+                FileOutputStream fichDst = new FileOutputStream(new File(destFich+nomFich));
+                
+                // Tant que le buffer n'est pas vide on continue de lire les données
+                while((n = entreeSocket.read(buffer)) >= 0) {
+                    // On les écrits dans le fichier destinaton
+                    fichDst.write(buffer,0,n);
+                    
+                }
+                // Le fichier a bien été copié
+                System.out.println("Fichier créé à la racine du fichier C:"
+                        + " dans le dossier BitTorrentBHV");
+                
+                // on ferme nous aussi la connexion
+                sssF.close();
+                sssB.close();
+                fichDst.close();
+            } catch (IOException e){
+            }    
+        }
+    }
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
